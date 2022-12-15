981cffc451d32ec94b3d0466ed694e328cea2908
komed3 <pk97@outlook.de>
2022-12-15 17:50:31 +0100

    rework + favicon

---
+++ inc/apm.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once __DIR__ . '/../config.php';
+
+    $DB = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT );
+
+    $DB->set_charset( DB_CHARSET );
+
+?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once __DIR__ . '/inc/config.php';
-    require_once __DIR__ . '/inc/classes/APM.php';
-
-    $APM = new APM();
-    $APM->run();
-
-?>
