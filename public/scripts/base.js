401a6a592536f993e15dd3419f6b711c31c9b952
komed3 <pk97@outlook.de>
2022-12-15 23:40:20 +0100

    add icons + basics + header

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -11,9 +11,55 @@ var baseurl = window.location.origin,
 
     };
 
+    var i18n = function(
+        text = ''
+    ) {
+
+        return text;
+
+    };
+
+    var dynContent = function() {
+
+        $( '[data-i18n]' ).each( function() {
+
+            $( this )
+                .html( i18n( $( this ).attr( 'data-i18n' ) ) )
+                .removeAttr( 'data-i18n' );
+
+        } );
+
+        $( '[data-title]' ).each( function() {
+
+            $( this )
+                .attr( 'title', i18n( $( this ).attr( 'data-title' ) ) )
+                .removeAttr( 'data-title' );
+
+        } );
+
+        $( '[data-placeholder]' ).each( function() {
+
+            $( this )
+                .attr( 'placeholder', i18n( $( this ).attr( 'data-placeholder' ) ) )
+                .removeAttr( 'data-placeholder' );
+
+        } );
+
+        $( '[data-href]' ).each( function() {
+
+            $( this )
+                .attr( 'href', baseurl + '/' + $( this ).attr( 'data-href' ) )
+                .removeAttr( 'data-href' );
+
+        } );
+
+    };
+
     $( document ).ready( function() {
 
         setLocale();
+
+        dynContent();
 
     } );
 
