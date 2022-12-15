d67675a6ac4a97814840724a64de284c96944bc1
komed3 <pk97@outlook.de>
2022-12-15 16:29:17 +0100

    Update APM.php

--- inc/classes/APM.php
+++ inc/classes/APM.php
@@ -6,12 +6,7 @@
 
             require_once __DIR__ . '/Coord.php';
             require_once __DIR__ . '/DB.php';
-
             require_once __DIR__ . '/Airport.php';
-
-            $a = new Airport( 'EDCJ' );
-
-            echo $a->coord( 'dms' );
 
         }
 
