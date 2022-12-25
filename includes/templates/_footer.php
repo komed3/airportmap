1a658996cf8c3143261eec1c4e69c9a0a94117cb
komed3 <pk97@outlook.de>
2022-12-25 18:24:35 +0100

    add footer warning

--- includes/templates/_footer.php
+++ includes/templates/_footer.php
@@ -1,3 +1,9 @@
+            <div id="warning">
+                <span><?php _i18n( 'site-warning' ); ?></span>
+            </div>
+            <div id="scroll-to-top" data-action="scroll-to-top" title="<?php _i18n( 'scroll-to-top' ); ?>">
+                <i class="icon">arrow_upward</i>
+            </div>
         </div>
     </body>
 </html>
