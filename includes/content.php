7f64c361b7c4cfe0cfc866ce8b32fbdf52b3cf8d
komed3 <pk97@outlook.de>
2022-12-25 15:51:55 +0100

    site header + logo

--- includes/content.php
+++ includes/content.php
@@ -1,8 +1,11 @@
 <?php
 
     $__site_canonical;
+
     $__site_title;
     $__site_desc;
+
+    $__site_class = [ 'apm' ];
 
     function _base_url(
         string $url = ''
@@ -26,11 +29,21 @@
 
     function site_header() {
 
+        global $__site_canonical, $__site_title, $__site_desc;
+
         ?>
             <link rel="canonical" href="<?php echo SITE . $__site_canonical; ?>" />
             <title><?php _i18n( $__site_title ?? 'site-title-default' ); ?> — Airportmap</title>
             <meta name="description" content="<?php _i18n( $__site_desc ?? 'site-desc-default' ); ?>" />
         <?php
+
+    }
+
+    function site_class() {
+
+        global $__site_class;
+
+        ?>class="<?php echo implode( ' ', $__site_class ); ?>"<?php
 
     }
 
--- includes/templates/_header.php
+++ includes/templates/_header.php
@@ -7,10 +7,17 @@
         <meta name="msapplication-TileColor" content="#603cba" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="apple-touch-icon" sizes="180x180" href="<?php _base_url( 'apple-touch-icon.png' ); ?>" />
+        <link rel="icon" type="image/png" sizes="48x48" href="<?php _base_url( 'favicon-48x48.png' ); ?>" />
         <link rel="icon" type="image/png" sizes="32x32" href="<?php _base_url( 'favicon-32x32.png' ); ?>" />
         <link rel="icon" type="image/png" sizes="16x16" href="<?php _base_url( 'favicon-16x16.png' ); ?>" />
         <link rel="manifest" href="<?php _base_url( 'site.webmanifest' ); ?>" />
         <link rel="mask-icon" href="<?php _base_url( 'safari-pinned-tab.svg' ); ?>" color="#7050aa" />
         <?php site_header(); ?>
     </head>
-    <body>
+    <body <?php site_class(); ?>>
+        <div id="wrapper">
+            <div id="header">
+                <a href="<?php _base_url(); ?>" title="<?php _i18n( 'site-title-default' ); ?>" class="site-logo">
+                    <img src="<?php _base_url( 'favicon-48x48.png' ); ?>" alt="" />
+                </a>
+            </div>
