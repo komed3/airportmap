471cb00e934f88a7ae635b68fc67e2cdd7f428f9
komed3 <pk97@outlook.de>
2022-12-30 11:50:25 +0100

    sigmet valid time

--- includes/api/sigmet_info.php
+++ includes/api/sigmet_info.php
@@ -24,7 +24,11 @@
         $infobox = [
             'title' => sigmet_hazard( $sigmet ),
             'subtitle' => $sigmet['name'],
-            'content' => '<div class="rawtxt">' . $sigmet['raw'] . '</div>',
+            'content' => '<div class="rawtxt">' . $sigmet['raw'] . '</div>
+            <h5>' . i18n( 'valid-from-to',
+                date( 'H:i', strtotime( $sigmet['valid_from'] ) ),
+                date( 'H:i', strtotime( $sigmet['valid_to'] ) )
+            ) . '</h5>',
             'link' => SITE . 'weather/sigmets',
             'linktext' => i18n( 'view-sigmets' )
         ];
