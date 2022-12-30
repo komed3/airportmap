91e683239c9614bc172538759f6efc2ee053be0e
komed3 <pk97@outlook.de>
2022-12-30 14:14:53 +0100

    SIGMET info FL

--- includes/api/sigmet_info.php
+++ includes/api/sigmet_info.php
@@ -24,6 +24,14 @@
         $valid_from = strtotime( $sigmet['valid_from'] );
         $valid_to = strtotime( $sigmet['valid_to'] );
 
+        $fl_msgkey = 'fl';
+
+        if( !empty( $fl_base = $sigmet['lo_1'] ?? ( $sigmet['lo_2'] ?? null ) ) )
+            $fl_msgkey .= '-from';
+
+        if( !empty( $fl_top = $sigmet['hi_1'] ?? ( $sigmet['hi_2'] ?? null ) ) )
+            $fl_msgkey .= '-to';
+
         $infobox = [
             'title' => sigmet_hazard( $sigmet ),
             'subtitle' => $sigmet['name'],
@@ -49,7 +57,14 @@
             <h5>
                 <i class="icon">warning</i>
                 <span>' . ucfirst( i18n( 'change-' . ( $sigmet['cng'] ?? 'NC' ) ) ) . '</span>
-            </h5>',
+            </h5>' . ( $fl_base || $fl_top ? '
+            <h5>
+                <i class="icon">flight_takeoff</i>
+                <span>' . ucfirst( i18n( $fl_msgkey,
+                    str_pad( (int) $fl_base / 100, 3, '0', STR_PAD_LEFT ),
+                    str_pad( (int) $fl_top / 100, 3, '0', STR_PAD_LEFT )
+                ) ) . '</span>
+            </h5>' : '' ),
             'link' => SITE . 'weather/sigmets',
             'linktext' => i18n( 'view-sigmets' )
         ];
