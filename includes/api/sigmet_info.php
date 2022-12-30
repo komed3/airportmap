f55a78bce35d866351a1c1eb441b61a93890f02a
komed3 <pk97@outlook.de>
2022-12-30 12:14:13 +0100

    SIGMET movement + change info

--- includes/api/sigmet_info.php
+++ includes/api/sigmet_info.php
@@ -25,10 +25,25 @@
             'title' => sigmet_hazard( $sigmet ),
             'subtitle' => $sigmet['name'],
             'content' => '<div class="rawtxt">' . $sigmet['raw'] . '</div>
-            <h5>' . i18n( 'valid-from-to',
-                date( 'H:i', strtotime( $sigmet['valid_from'] ) ),
-                date( 'H:i', strtotime( $sigmet['valid_to'] ) )
-            ) . '</h5>',
+            <hr />
+            <h5>
+                <i class="icon">schedule</i>
+                <span>' . i18n( 'valid-from-to',
+                    date( 'H:i', strtotime( $sigmet['valid_from'] ) ),
+                    date( 'H:i', strtotime( $sigmet['valid_to'] ) )
+                ) . '</span>
+            </h5>
+            <h5>
+                <i class="icon">near_me</i>
+                <span>' . ( $sigmet['dir'] ? i18n( 'movement',
+                    i18n( 'dir-' . $sigmet['dir'] ),
+                    (int) $sigmet['spd'] . '&#8239;kt'
+                ) : i18n( 'stationary' ) ) . '</span>
+            </h5>
+            <h5>
+                <i class="icon">warning</i>
+                <span>' . ucfirst( i18n( 'change-' . ( $sigmet['cng'] ?? 'NC' ) ) ) . '</span>
+            </h5>',
             'link' => SITE . 'weather/sigmets',
             'linktext' => i18n( 'view-sigmets' )
         ];
--- includes/weather.php
+++ includes/weather.php
@@ -4,10 +4,10 @@
         array $sigmet
     ) {
 
-        return implode( ' ', array_filter( [
+        return ucfirst( implode( ' ', array_filter( [
             i18n_save( 'qualifier-' . $sigmet['qualifier'] ),
             i18n_save( 'hazard-' . $sigmet['hazard'] )
-        ] ) );
+        ] ) ) );
 
     }
 
