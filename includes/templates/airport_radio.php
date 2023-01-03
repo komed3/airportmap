98dbf49cf524a29c054081ddbb724ec464e99c42
komed3 <pk97@outlook.de>
2023-01-03 11:47:15 +0100

    airport radio page (lists empty)

---
+++ includes/templates/airport_radio.php
@@ -0,0 +1,30 @@
+<?php
+
+    global $DB, $airport;
+
+    if( empty( $airport ) ) {
+
+        __404();
+
+    }
+
+?>
+<div class="airport-radio content-normal">
+    <h2 class="secondary-headline"><?php _i18n( 'airport-frequencies' ); ?></h2>
+    <?php if( count( $radios = $DB->query( '
+        SELECT  *
+        FROM    ' . DB_PREFIX . 'frequency
+        WHERE   airport = "' . $airport['ICAO'] . '"
+    ' )->fetch_all( MYSQLI_ASSOC ) ) > 0 ) { ?>
+        ...
+    <?php } else { ?>
+        <p><?php _i18n( 'airport-frequencies-empty' ); ?></p>
+    <?php } if( count( $navaids = $DB->query( '
+        SELECT  *
+        FROM    ' . DB_PREFIX . 'navaid
+        WHERE   airport = "' . $airport['ICAO'] . '"
+    ' )->fetch_all( MYSQLI_ASSOC ) ) > 0 ) { ?>
+        <h2 class="secondary-headline"><?php _i18n( 'airport-navaids' ); ?></h2>
+        ...
+    <?php } ?>
+</div>
