94a1de8a4b9de2bf5c11701be2fb5f436189dc19
komed3 <pk97@outlook.de>
2023-01-04 16:04:06 +0100

    nearby tabs nav

--- includes/templates/airport_nearby.php
+++ includes/templates/airport_nearby.php
@@ -1,6 +1,6 @@
 <?php
 
-    global $DB, $airport;
+    global $DB, $path, $airport, $base;
 
     if( empty( $airport ) ) {
 
@@ -8,17 +8,45 @@
 
     }
 
+    $path[3] = $path[3] ?? 'nearest';
+
 ?>
 <div class="airport-nearby content-normal">
-    <?php _map( [
-        'type' => 'airport',
-        'navaids' => true,
-        'supress_sigmets' => false,
-        'supress_day_night' => false,
-        'position' => [
-            'lat' => $airport['lat'],
-            'lon' => $airport['lon'],
-            'zoom' => 9
-        ]
-    ], 'minimal-ui' ); ?>
+    <?php
+
+        _map( [
+            'type' => 'airport',
+            'navaids' => true,
+            'supress_sigmets' => false,
+            'supress_day_night' => false,
+            'position' => [
+                'lat' => $airport['lat'],
+                'lon' => $airport['lon'],
+                'zoom' => 9
+            ]
+        ], 'minimal-ui' );
+
+        _site_nav( [ null, [
+            'i18n' => 'nearby-nearest',
+            'url' => $base . 'nearby/nearest',
+            'check' => 'nearest'
+        ], [
+            'i18n' => 'nearby-largest',
+            'url' => $base . 'nearby/largest',
+            'check' => 'largest'
+        ], [
+            'i18n' => 'nearby-airports',
+            'url' => $base . 'nearby/airports',
+            'check' => 'airports'
+        ], [
+            'i18n' => 'nearby-service',
+            'url' => $base . 'nearby/service',
+            'check' => 'service'
+        ], [
+            'i18n' => 'nearby-all',
+            'url' => $base . 'nearby/all',
+            'check' => 'all'
+        ], null ], 'site-tabs content-normal', 3 );
+
+    ?>
 </div>
