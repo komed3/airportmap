6ed6e20b370f0abb28198ed4e9d62f164c764c6a
komed3 <pk97@outlook.de>
2023-01-06 14:47:56 +0100

    404 page + background image

--- includes/templates/404.php
+++ includes/templates/404.php
@@ -1,3 +1,19 @@
-<?php _header(); ?>
+<?php
 
+    add_resource( '404', 'css', '404.css' );
+
+    _header();
+
+?>
+<div class="content-full error-404">
+    <div class="credits"><?php _i18n(
+        '404-credits',
+        '<a href="https://pixabay.com/users/tobiasrehbein-11751606">Tobias Rehbein</a>',
+        '<a href="https://pixabay.com">Pixabay</a>'
+    ); ?></div>
+    <h1><?php _i18n( '404-teaser' ); ?></h1>
+    <h2><?php _i18n( '404-headline' ); ?></h2>
+    <p><?php _i18n( '404-text' ); ?></p>
+    <?php load_tpl_part( '_searchform' ); ?>
+</div>
 <?php _footer(); ?>
