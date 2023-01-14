4e062600a6c1d3ac6793bf759c6d702633692995
komed3 <pk97@outlook.de>
2023-01-14 11:31:14 +0100

    about APM

--- includes/templates/about.php
+++ includes/templates/about.php
@@ -19,5 +19,20 @@
         ); ?></div>
     </div>
     <h1 class="primary-headline"><?php _i18n( 'about-title' ); ?></h1>
+    <div class="content-normal">
+        <p class="first"><?php _i18n( 'site-welcome-text', __number( AIRPORT_ALL ) ); ?></p>
+        <p><?php _i18n( 'about-code' ); ?></p>
+        <h2 class="secondary-headline"><?php _i18n( 'about-goal' ); ?></h2>
+        <p><?php _i18n( 'about-goal-primary' ); ?></p>
+        <p><?php _i18n( 'about-goal-secondary' ); ?></p>
+        <p class="warn"><?php _i18n( 'site-warning' ); ?></p>
+        <h2 class="secondary-headline"><?php _i18n( 'about-source' ); ?></h2>
+        <p><?php _i18n( 'about-source-sources' ); ?></p>
+        <p><?php _i18n( 'about-source-airport' ); ?></p>
+        <p><?php _i18n( 'about-source-image' ); ?></p>
+        <p><?php _i18n( 'about-source-weather' ); ?></p>
+        <p><?php _i18n( 'about-source-sigmet' ); ?></p>
+        <p><?php _i18n( 'about-source-map' ); ?></p>
+    </div>
 </div>
 <?php _footer(); ?>
