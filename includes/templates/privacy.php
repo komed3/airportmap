7945ff8b6e5dcc8a961135c4ee59074776a94e39
komed3 <pk97@outlook.de>
2023-01-18 11:28:28 +0100

    privacy content + cookies list

--- includes/templates/privacy.php
+++ includes/templates/privacy.php
@@ -15,11 +15,20 @@
     <p class="first"><?php _i18n( 'privacy-consens' ); ?></p>
     <h2><?php _i18n( 'privacy-storage-headline' ); ?></h2>
     <p><?php _i18n( 'privacy-storage' ); ?></p>
+    <p><?php _i18n( 'privacy-usage' ); ?></p>
     <h2><?php _i18n( 'privacy-services-headline' ); ?></h2>
     <p><?php _i18n( 'privacy-services' ); ?></p>
     <p><?php _i18n( 'privacy-geolocation' ); ?></p>
     <h2><?php _i18n( 'privacy-cookies-headline' ); ?></h2>
     <p><?php _i18n( 'privacy-cookies' ); ?></p>
+    <?php foreach( [
+        'apm_lastpos', 'apm_map_type', 'apm_sigmet',
+        'apm_day_night', 'locale'
+    ] as $cookie ) { ?>
+        <p><code><?php echo $cookie; ?></code> <?php _i18n( 'privacy-cookie-' . $cookie ); ?></p>
+    <?php } ?>
+    <h2><?php _i18n( 'privacy-disclaimer-headline' ); ?></h2>
+    <p><?php _i18n( 'privacy-disclaimer' ); ?></p>
     <p><?php _i18n( 'privacy-contact' ); ?></p>
 </div>
 <?php _footer(); ?>
