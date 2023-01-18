d30093405110610ff1d50ba71c33912388261f63
komed3 <pk97@outlook.de>
2023-01-18 10:51:44 +0100

    add privacy content

--- includes/templates/privacy.php
+++ includes/templates/privacy.php
@@ -1,5 +1,7 @@
 <?php
 
+    add_resource( 'text', 'css', 'text.css' );
+
     $__site_canonical = $base . 'privacy';
 
     $__site_title = i18n( 'site-privacy' );
@@ -8,7 +10,16 @@
     _header();
 
 ?>
-<div class="content-normal privacy">
+<div class="content-normal privacy text-content">
     <h1><?php _i18n( 'site-privacy' ); ?></h1>
+    <p class="first"><?php _i18n( 'privacy-consens' ); ?></p>
+    <h2><?php _i18n( 'privacy-storage-headline' ); ?></h2>
+    <p><?php _i18n( 'privacy-storage' ); ?></p>
+    <h2><?php _i18n( 'privacy-services-headline' ); ?></h2>
+    <p><?php _i18n( 'privacy-services' ); ?></p>
+    <p><?php _i18n( 'privacy-geolocation' ); ?></p>
+    <h2><?php _i18n( 'privacy-cookies-headline' ); ?></h2>
+    <p><?php _i18n( 'privacy-cookies' ); ?></p>
+    <p><?php _i18n( 'privacy-contact' ); ?></p>
 </div>
 <?php _footer(); ?>
