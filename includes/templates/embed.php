ae48d0751e4e25cdf04a03e08b83ee727da78807
komed3 <pk97@outlook.de>
2023-01-18 14:11:27 +0100

    embed intro

--- includes/templates/embed.php
+++ includes/templates/embed.php
@@ -1,5 +1,7 @@
 <?php
 
+    add_resource( 'text', 'css', 'text.css' );
+
     $__site_canonical = $base . 'embed';
 
     $__site_title = i18n( 'embed-title' );
@@ -8,7 +10,9 @@
     _header();
 
 ?>
-<div class="content-normal embed">
+<div class="content-normal embed text-content">
     <h1><?php _i18n( 'embed-title' ); ?></h1>
+    <p class="first"><?php _i18n( 'embed-intro' ); ?></p>
+    <p><?php _i18n( 'embed-usage', base_url( 'privacy' ) ); ?></p>
 </div>
 <?php _footer(); ?>
