dc5ec359d4e312e8df60b417470ba7645a45f8f6
komed3 <pk97@outlook.de>
2023-01-19 18:18:59 +0100

    cookie banner content + CSS

--- includes/templates/_cookie.php
+++ includes/templates/_cookie.php
@@ -5,8 +5,7 @@
         <h1><?php _i18n( 'cookie-title' ) ?></h1>
         <p><?php _i18n( 'cookie-text' ); ?></p>
         <div class="cookie-save">
-            <span><?php _i18n( 'cookie-label' ); ?></span>
-            <a href="#" data-action="cookie-accept"><?php _i18n( 'cookie-accept' ); ?></a>
+            <a href="#" class="accept" data-action="cookie-accept"><?php _i18n( 'cookie-accept' ); ?></a>
             <a href="#" data-action="cookie-reject"><?php _i18n( 'cookie-reject' ); ?></a>
             <a href="<?php _base_url( 'privacy' ); ?>"><?php _i18n( 'site-privacy' ); ?></a>
         </div>
