ff3a926fb1bfb52ed705f71bbb5deedaf391fb80
komed3 <pk97@outlook.de>
2023-01-20 14:25:26 +0100

    Update sitemap.php

--- includes/api/sitemap.php
+++ includes/api/sitemap.php
@@ -118,7 +118,10 @@
         </sitemap>';
 
         file_put_contents( BASE . 'sitemap-' . $i . '.xml', '<?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" ' .
+            'xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" ' .
+            'xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" ' .
+            'xmlns:xhtml="http://www.w3.org/1999/xhtml">
             ' . implode( '', $chunk ) . '
         </urlset>' );
 
