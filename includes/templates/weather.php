701a0f432086d1228b50c8a3027e5911fc433124
komed3 <pk97@outlook.de>
2023-01-23 22:36:59 +0100

    weather map >> set start position

--- includes/templates/weather.php
+++ includes/templates/weather.php
@@ -11,19 +11,6 @@
         ORDER BY tier DESC
     ' )->fetch_all( MYSQLI_ASSOC );
 
-    $position = $DB->query( '
-        SELECT  MIN( a.lat ) AS lat_min,
-                MIN( a.lon ) AS lon_min,
-                MAX( a.lat ) AS lat_max,
-                MAX( a.lon ) AS lon_max,
-                AVG( a.lat ) AS lat_avg,
-                AVG( a.lon ) AS lon_avg
-        FROM    ' . DB_PREFIX . 'metar m,
-                ' . DB_PREFIX . 'airport a
-        WHERE   m.station = a.ICAO
-        AND     m.reported >= DATE_SUB( NOW(), INTERVAL 1 DAY )
-    ' )->fetch_object();
-
     $__site_canonical = $base . 'airports/weather';
 
     $__site_title = i18n( 'weather-title' );
@@ -39,9 +26,10 @@
         'type' => 'weather',
         'navaids' => false,
         'supress_day_night' => true,
-        'fit_bounds' => [
-            [ $position->lat_min, $position->lon_min ],
-            [ $position->lat_max, $position->lon_max ]
+        'position' => [
+            'lat' => 40.7,
+            'lon' => -74,
+            'zoom' => 5
         ]
     ], 'minimal-ui windbug' ); ?>
     <h1 class="primary-headline">
