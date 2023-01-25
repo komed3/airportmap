bdd2b729897449af1cacacca157743157dc142e3
komed3 <pk97@outlook.de>
2023-01-25 11:14:45 +0100

    list A-Z

--- includes/templates/list.php
+++ includes/templates/list.php
@@ -1,20 +1,73 @@
 <?php
 
-    $letter = substr( strtoupper( trim( $path[1] ?? '*' ) ), 0, 1 );
+    $letter = substr( strtoupper( trim( $path[1] ?? '' ) ), 0, 1 );
 
-    if( strpos( '*#ABCDEFGHIJKLMNOPQRSTUVWXYZ', $letter ) === false ) {
+    if( strlen( $letter ) == 1 ) {
 
-        __404();
+        if( strpos( '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', $letter ) === false ) {
+
+            __404();
+
+        }
+
+        $query = ' ICAO LIKE "' . $letter . '%" ';
+
+        $__site_canonical = 'list/' . $letter;
+
+        $__site_title = i18n( 'list-letter-title', $letter );
+        $__site_desc = i18n( 'list-letter-desc', $letter );
+
+    } else {
+
+        $query = '1';
+
+        $__site_canonical = 'list';
+
+        $__site_title = i18n( 'list-title' );
+        $__site_desc = i18n( 'list-desc' );
 
     }
 
-    $__site_canonical = 'list';
+    $airports = $DB->query( '
+        SELECT   *
+        FROM     ' . DB_PREFIX . 'airport
+        WHERE    ' . $query . '
+        ORDER BY ICAO ASC
+    ' )->fetch_all( MYSQLI_ASSOC );
 
-    $__site_title = i18n( 'list-title', $letter, $_count );
-    $__site_desc = i18n( 'list-desc', $letter, $_count );
+    $count = count( $airports );
+    $_count = __number( $count );
 
     _header();
 
 ?>
-
+<div class="content-full list">
+    <?php _site_nav(
+        array_merge( [ [
+            'i18n' => 'list-all',
+            'url' => 'list',
+            'check' => ''
+        ] ], array_map( function( $l ) {
+            return [
+                'text' => $l,
+                'url' => 'list/' . $l,
+                'check' => $l
+            ];
+        }, str_split(
+            '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        ) ) ),
+        'site-tabs content-normal', 1
+    ); ?>
+    <h1 class="primary-headline">
+        <i class="icon">tag</i>
+        <span><?php echo $__site_title; ?></span>
+        <b><?php echo $_count; ?></b>
+    </h1>
+    <div class="content-normal">
+        <?php _airport_list(
+            $airports, $path[2] ?? 1,
+            $letter ? 'list/' . $letter : 'list'
+        ); ?>
+    </div>
+</div>
 <?php _footer(); ?>
