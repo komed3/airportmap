04023aefe6024124821494a8a2167c984c007731
komed3 <pk97@outlook.de>
2023-01-26 11:09:20 +0100

    simplify sitemap index

--- includes/api/sitemap.php
+++ includes/api/sitemap.php
@@ -37,22 +37,6 @@
 
     }
 
-    /* airports */
-
-    foreach( array_column( $DB->query( '
-        SELECT   ICAO
-        FROM     ' . DB_PREFIX . 'airport
-        ORDER BY tier DESC
-    ' )->fetch_all( MYSQLI_ASSOC ), 'ICAO' ) as $airport ) {
-
-        sitemap_entry( 'airport/' . $airport . '/info' );
-        sitemap_entry( 'airport/' . $airport . '/weather' );
-        sitemap_entry( 'airport/' . $airport . '/nearby' );
-        sitemap_entry( 'airport/' . $airport . '/radio' );
-        sitemap_entry( 'airport/' . $airport . '/runways' );
-
-    }
-
     /* regions */
 
     foreach( [ 'continent', 'country', 'region', 'ICAO' ] as $type ) {
@@ -96,11 +80,23 @@
 
     }
 
+    /* airports */
+
+    foreach( array_column( $DB->query( '
+        SELECT   ICAO
+        FROM     ' . DB_PREFIX . 'airport
+        ORDER BY tier DESC
+    ' )->fetch_all( MYSQLI_ASSOC ), 'ICAO' ) as $airport ) {
+
+        sitemap_entry( 'airport/' . $airport );
+
+    }
+
     /* build index */
 
     $index = [];
 
-    foreach( array_chunk( $sitemap, 10000 ) as $i => $chunk ) {
+    foreach( array_chunk( $sitemap, 5000 ) as $i => $chunk ) {
 
         $index[] = '<sitemap>
             <loc>' . SITE . 'sitemap-' . $i . '.xml</loc>
