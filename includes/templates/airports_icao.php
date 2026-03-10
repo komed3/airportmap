423986f39097a801d845e197f64743926c908725
Paul Köhler <webmaster@komed3.de>
2026-03-10 11:12:15 +0100

    fix ICAO counts

--- includes/templates/airports_icao.php
+++ includes/templates/airports_icao.php
@@ -4,11 +4,7 @@
         SELECT  *
         FROM    ' . DB_PREFIX . 'ICAO
         WHERE   code = "' . ( $path[2] ?? '' ) . '"
-    ' ) )->num_rows == 0 ) {
-
-        __404();
-
-    }
+    ' ) )->num_rows == 0 ) __404();
 
     $ICAO = $ICAO->fetch_object();
     $plain = str_replace( '*', '', $ICAO->code );
@@ -16,9 +12,7 @@
     $countries = $regions = [];
 
     foreach( explode( '|', $ICAO->regions ) as $r ) {
-
         ${ strlen( $r ) == 2 ? 'countries' : 'regions' }[] = $r;
-
     }
 
     $regions_query = ' AND ( country IN ( "' .
@@ -45,21 +39,62 @@
         ' . $regions_query . '
     ' )->fetch_object();
 
-    $count = count( $airports );
-    $_count = __number( $count );
+    $count = 0;
+    $links = [];
+
+    if( strlen( $ICAO->code ) == 1 ) {
+
+        $rows = $DB->query( '
+            SELECT   *
+            FROM     ICAO
+            WHERE    code LIKE "' . $plain . '%"
+            AND      code != "' . $plain . '"
+        ' )->fetch_all( MYSQLI_ASSOC );
+
+        foreach( $rows as $row ) {
+
+            $c = $r = [];
+
+            foreach( explode( '|', $row['regions'] ) as $reg ) {
+                ${ strlen( $reg ) == 2 ? 'c' : 'r' }[] = $reg;
+            }
+
+            $cnt = $DB->query( '
+                SELECT   ICAO
+                FROM     ' . DB_PREFIX . 'airport a
+                WHERE    ICAO LIKE "' . str_replace( '*', '', $row['code'] ) . '%"
+                AND    ( country IN ( "' .
+                    implode( '", "', $c ) . '" ) OR region IN ( "' .
+                    implode( '", "', $r ) . '" )
+                )
+            ' )->num_rows;
+
+            $links[] = [
+                'page' => $row['code'],
+                'name' => $row['name'],
+                'cnt' => $cnt
+            ];
+
+            $count += $cnt;
+
+        }
+
+    } else {
+
+        $count = count( $airports );
+
+    }
 
     $breadcrumbs = [ [ 'world' ] ];
 
     for( $i = 1; $i <= strlen( $ICAO->code ); $i++ ) {
-
         $breadcrumbs[] = [ 'ICAO', substr( $ICAO->code, 0, $i ) ];
-
     }
 
     $__site_canonical = 'airports/ICAO/' . $ICAO->code;
 
-    $__site_title = i18n( 'airports-icao-title', $ICAO->code, $ICAO->name, $_count );
-    $__site_desc = i18n( 'airports-icao-desc', $ICAO->code, $ICAO->name, $_count );
+    $__site_title = i18n( 'airports-icao-title', $ICAO->code, $ICAO->name );
+    $__site_desc = i18n( 'airports-icao-desc', $ICAO->code, $ICAO->name );
 
     add_resource( 'region', 'css', 'region.min.css' );
 
@@ -85,26 +120,10 @@
     <h1 class="primary-headline">
         <i class="icon">language</i>
         <span><?php echo $__site_title; ?></span>
-        <b><?php echo $_count; ?></b>
+		<b><?php echo __number( $count ); ?></b>
     </h1>
     <?php _breadcrumbs( $breadcrumbs ); ?>
-    <?php if( strlen( $ICAO->code ) == 1 ) {
-
-        _pagelist( 'airports/ICAO', $DB->query( '
-            SELECT   i.code AS page,
-                     CONCAT( i.code, ": ", i.name ) AS name,
-                     COUNT( a.ICAO ) AS cnt
-            FROM     ' . DB_PREFIX . 'ICAO i,
-                     ' . DB_PREFIX . 'airport a
-            WHERE    i.code LIKE "' . $ICAO->code . '%"
-            AND      i.code != "' . $ICAO->code . '"
-            AND      a.ICAO LIKE CONCAT( REPLACE( i.code, "*", "" ), "%" )
-            ' . $regions_query . '
-            GROUP BY i.code
-            ORDER BY i.code ASC
-        ' )->fetch_all( MYSQLI_ASSOC ) );
-
-    } ?>
+    <?php if( count( $links ) > 0 ) _pagelist( 'airports/ICAO', $links ); ?>
     <div class="content-normal">
         <?php _airport_list(
             $airports, $path[3] ?? 1,
