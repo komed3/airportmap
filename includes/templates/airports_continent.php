f15abc64e35bcf2d6aad433543b80078d6d93c91
komed3 <webmaster@komed3.de>
2026-09-13 23:58:45 +0200

    update airports_continent.php

--- includes/templates/airports_continent.php
+++ includes/templates/airports_continent.php
@@ -1,77 +1,73 @@
 <?php
 
-    if( ( $continent = $DB->query( '
-        SELECT  *
-        FROM    ' . DB_PREFIX . 'continent
-        WHERE   code = "' . ( $path[2] ?? '' ) . '"
-    ' ) )->num_rows == 0 ) {
+  if ( ( $continent = $DB->query( '
+    SELECT  *
+    FROM    ' . DB_PREFIX . 'continent
+    WHERE   code = "' . ( $path[ 2 ] ?? '' ) . '"
+  ' ) )->num_rows == 0 ) __404();
 
-        __404();
+  $continent = $continent->fetch_object();
 
-    }
+  $list = $DB->query( '
+    SELECT   c.code AS page,
+             c.name AS name,
+             COUNT( a.ICAO ) AS cnt
+    FROM     ' . DB_PREFIX . 'country c,
+             ' . DB_PREFIX . 'airport a
+    WHERE    c.continent = "' . $continent->code . '"
+    AND      a.country = c.code
+    GROUP BY c.code
+    ORDER BY c.code ASC
+  ' )->fetch_all( MYSQLI_ASSOC );
 
-    $continent = $continent->fetch_object();
+  $position = $DB->query( '
+    SELECT  MIN( lat ) AS lat_min,
+            MIN( lon ) AS lon_min,
+            MAX( lat ) AS lat_max,
+            MAX( lon ) AS lon_max,
+            AVG( lat ) AS lat_avg,
+            AVG( lon ) AS lon_avg
+    FROM    ' . DB_PREFIX . 'airport
+    WHERE   continent = "' . $continent->code . '"
+  ' )->fetch_object();
 
-    $list = $DB->query( '
-        SELECT   c.code AS page,
-                 c.name AS name,
-                 COUNT( a.ICAO ) AS cnt
-        FROM     ' . DB_PREFIX . 'country c,
-                 ' . DB_PREFIX . 'airport a
-        WHERE    c.continent = "' . $continent->code . '"
-        AND      a.country = c.code
-        GROUP BY c.code
-        ORDER BY c.code ASC
-    ' )->fetch_all( MYSQLI_ASSOC );
+  $count = array_sum( array_column( $list, 'cnt' ) );
+  $_count = __number( $count );
 
-    $position = $DB->query( '
-        SELECT  MIN( lat ) AS lat_min,
-                MIN( lon ) AS lon_min,
-                MAX( lat ) AS lat_max,
-                MAX( lon ) AS lon_max,
-                AVG( lat ) AS lat_avg,
-                AVG( lon ) AS lon_avg
-        FROM    ' . DB_PREFIX . 'airport
-        WHERE   continent = "' . $continent->code . '"
-    ' )->fetch_object();
+  $__site_canonical = 'airports/continent/' . $continent->code;
 
-    $count = array_sum( array_column( $list, 'cnt' ) );
-    $_count = __number( $count );
+  $__site_title = i18n( 'airports-continent-title', $continent->name, $continent->code, $_count );
+  $__site_desc = i18n( 'airports-continent-desc', $continent->name, $continent->code, $_count );
 
-    $__site_canonical = 'airports/continent/' . $continent->code;
+  add_resource( 'region', 'css', 'region.min.css' );
 
-    $__site_title = i18n( 'airports-continent-title', $continent->name, $continent->code, $_count );
-    $__site_desc = i18n( 'airports-continent-desc', $continent->name, $continent->code, $_count );
-
-    add_resource( 'region', 'css', 'region.min.css' );
-
-    _header();
+  _header();
 
 ?>
 <div class="region">
-    <?php _map( [
-        'type' => 'airport',
-        'navaids' => false,
-        'waypoints' => false,
-        'supress_sigmets' => true,
-        'supress_day_night' => true,
-        'query' => [
-            'continent' => $continent->code
-        ],
-        'fit_bounds' => [
-            [ $position->lat_min, $position->lon_min ],
-            [ $position->lat_max, $position->lon_max ]
-        ]
-    ], 'minimal-ui' ); ?>
-    <h1 class="primary-headline">
-        <i class="icon">language</i>
-        <span><?php echo $continent->name; ?></span>
-        <b><?php echo $_count; ?></b>
-    </h1>
-    <?php _breadcrumbs( [
-        [ 'world' ],
-        [ 'continent', $continent->code ]
-    ] ); ?>
-    <?php _pagelist( 'airports/country', $list ); ?>
+  <?php _map( [
+    'type' => 'airport',
+    'navaids' => false,
+    'waypoints' => false,
+    'supress_sigmets' => true,
+    'supress_day_night' => true,
+    'query' => [
+      'continent' => $continent->code
+    ],
+    'fit_bounds' => [
+      [ $position->lat_min, $position->lon_min ],
+      [ $position->lat_max, $position->lon_max ]
+    ]
+  ], 'minimal-ui' ); ?>
+  <h1 class="primary-headline">
+    <i class="icon">language</i>
+    <span><?php echo $continent->name; ?></span>
+    <b><?php echo $_count; ?></b>
+  </h1>
+  <?php _breadcrumbs( [
+    [ 'world' ],
+    [ 'continent', $continent->code ]
+  ] ); ?>
+  <?php _pagelist( 'airports/country', $list ); ?>
 </div>
 <?php _footer(); ?>
