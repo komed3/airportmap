f564200b759f4253d1bd2a9546d8c3d21550a271
komed3 <webmaster@komed3.de>
2026-09-13 23:47:59 +0200

    update _cookie.php, _footer.php and _header.php

--- includes/templates/_footer.php
+++ includes/templates/_footer.php
@@ -1,104 +1,104 @@
-            </div>
-            <div id="footer">
-                <div class="column">
-                    <a href="<?php _base_url(); ?>" title="<?php _i18n( 'site-title-default' ); ?>" class="site-logo">
-                        <img src="<?php _base_url( 'favicon-48x48.png' ); ?>" alt="" />
-                    </a>
-                    <h3>Airportmap</h3>
-                    <p class="about"><?php _i18n( 'footer-about', __number( floor( AIRPORT_ALL / 500 ) * 500 ) ); ?></p>
-                    <p class="credits"><?php _i18n( 'footer-credits', date( 'Y' ) ); ?></p>
-                </div>
-                <div class="column">
-                    <h3><?php _i18n( 'footer-headline-discover' ); ?></h3>
-                    <?php _site_nav( [ [
-                        'i18n' => 'footer-nav-airports',
-                        'url' => 'airports'
-                    ], [
-                        'i18n' => 'footer-nav-list',
-                        'url' => 'list'
-                    ], [
-                        'i18n' => 'footer-nav-weather',
-                        'url' => 'weather'
-                    ], [
-                        'i18n' => 'footer-nav-sigmets',
-                        'url' => 'weather/sigmets'
-                    ], [
-                        'i18n' => 'footer-nav-traffic',
-                        'url' => 'traffic'
-                    ], [
-                        'i18n' => 'footer-nav-vicinity',
-                        'url' => 'vicinity'
-                    ] ], 'footer-nav', -1 ); ?>
-                </div>
-                <div class="column">
-                    <h3><?php _i18n( 'footer-headline-developer' ); ?></h3>
-                    <?php _site_nav( [ [
-                        'i18n' => 'footer-nav-about',
-                        'url' => 'about'
-                    ], [
-                        'i18n' => 'footer-nav-data',
-                        'url' => 'data'
-                    ], [
-                        'i18n' => 'footer-nav-embed',
-                        'url' => 'embed'
-                    ], [
-                        'i18n' => 'footer-nav-stats',
-                        'url' => 'stats'
-                    ], [
-                        'i18n' => 'footer-nav-github',
-                        'external' => 'https://github.com/komed3/airportmap'
-                    ] ], 'footer-nav', -1 ); ?>
-                </div>
-                <div class="column">
-                    <h3><?php _i18n( 'footer-headline-support' ); ?></h3>
-                    <?php _site_nav( [ [
-                        'i18n' => 'site-privacy',
-                        'url' => 'privacy'
-                    ], [
-                        'i18n' => 'footer-nav-issues',
-                        'external' => 'https://github.com/komed3/airportmap/issues'
-                    ], [
-                        'i18n' => 'footer-nav-donate',
-                        'external' => 'https://github.com/komed3/airportmap'
-                    ] ], 'footer-nav', -1 ); ?>
-                    <h3><?php _i18n( 'footer-headline-settings' ); ?></h3>
-                    <div class="language-selector">
-                        <select data-action="select-language">
-                            <?php foreach( LANGUAGES as $lng ) { ?>
-                                <option value="<?php echo $lng; ?>" <?php if( $lng == i18n_locale() ) { ?>selected<?php } ?>>
-                                    <?php _i18n( 'site-language-' . $lng ); ?>
-                                </option>
-                            <?php } ?>
-                        </select>
-                        <i class="icon">language</i>
-                    </div>
-                    <div class="theme-selector theme-<?php echo $_COOKIE['theme'] ?? 'light'; ?>">
-                        <a href="#" data-action="theme" theme="light">
-                            <i class="icon">light_mode</i>
-                            <span class="label"><?php _i18n( 'theme-light' ); ?></span>
-                        </a>
-                        <a href="#" data-action="theme" theme="dark">
-                            <i class="icon">dark_mode</i>
-                            <span class="label"><?php _i18n( 'theme-dark' ); ?></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="share">
-                    <i class="icon">share</i>
-                    <span class="label"><?php _i18n( 'share-it' ); ?></span>
-                    <?php _share_links(); ?>
-                </div>
-            </div>
-            <div id="warning">
-                <span><?php _i18n( 'site-warning' ); ?></span>
-            </div>
-            <div id="scroll-to-top" data-action="scroll-to-top" title="<?php _i18n( 'scroll-to-top' ); ?>">
-                <i class="icon">arrow_upward</i>
-            </div>
-            <?php if( !isset( $_COOKIE['cookie_test'] ) ) {
-                load_tpl_part( '_cookie' );
-            } ?>
+      </div>
+      <div id="footer">
+        <div class="column">
+          <a href="<?php _base_url(); ?>" title="<?php _i18n( 'site-title-default' ); ?>" class="site-logo">
+            <img src="<?php _base_url( 'favicon-48x48.png' ); ?>" alt="" />
+          </a>
+          <h3>Airportmap</h3>
+          <p class="about"><?php _i18n( 'footer-about', __number( floor( AIRPORT_ALL / 500 ) * 500 ) ); ?></p>
+          <p class="credits"><?php _i18n( 'footer-credits', date( 'Y' ) ); ?></p>
         </div>
-        <?php _site_end(); ?>
-    </body>
+        <div class="column">
+          <h3><?php _i18n( 'footer-headline-discover' ); ?></h3>
+          <?php _site_nav( [ [
+            'i18n' => 'footer-nav-airports',
+            'url' => 'airports'
+          ], [
+            'i18n' => 'footer-nav-list',
+            'url' => 'list'
+          ], [
+            'i18n' => 'footer-nav-weather',
+            'url' => 'weather'
+          ], [
+            'i18n' => 'footer-nav-sigmets',
+            'url' => 'weather/sigmets'
+          ], [
+            'i18n' => 'footer-nav-traffic',
+            'url' => 'traffic'
+          ], [
+            'i18n' => 'footer-nav-vicinity',
+            'url' => 'vicinity'
+          ] ], 'footer-nav', -1 ); ?>
+        </div>
+        <div class="column">
+          <h3><?php _i18n( 'footer-headline-developer' ); ?></h3>
+          <?php _site_nav( [ [
+            'i18n' => 'footer-nav-about',
+            'url' => 'about'
+          ], [
+            'i18n' => 'footer-nav-data',
+            'url' => 'data'
+          ], [
+            'i18n' => 'footer-nav-embed',
+            'url' => 'embed'
+          ], [
+            'i18n' => 'footer-nav-stats',
+            'url' => 'stats'
+          ], [
+            'i18n' => 'footer-nav-github',
+            'external' => 'https://github.com/komed3/airportmap'
+          ] ], 'footer-nav', -1 ); ?>
+        </div>
+        <div class="column">
+          <h3><?php _i18n( 'footer-headline-support' ); ?></h3>
+          <?php _site_nav( [ [
+            'i18n' => 'site-privacy',
+            'url' => 'privacy'
+          ], [
+            'i18n' => 'footer-nav-issues',
+            'external' => 'https://github.com/komed3/airportmap/issues'
+          ], [
+            'i18n' => 'footer-nav-donate',
+            'external' => 'https://github.com/komed3/airportmap'
+          ] ], 'footer-nav', -1 ); ?>
+          <h3><?php _i18n( 'footer-headline-settings' ); ?></h3>
+          <div class="language-selector">
+            <select data-action="select-language">
+              <?php foreach ( LANGUAGES as $lng ) { ?>
+                <option value="<?php echo $lng; ?>" <?php if ( $lng == i18n_locale() ) { ?>selected<?php } ?>>
+                  <?php _i18n( 'site-language-' . $lng ); ?>
+                </option>
+              <?php } ?>
+            </select>
+            <i class="icon">language</i>
+          </div>
+          <div class="theme-selector theme-<?php echo $_COOKIE[ 'theme' ] ?? 'light'; ?>">
+            <a href="#" data-action="theme" theme="light">
+              <i class="icon">light_mode</i>
+              <span class="label"><?php _i18n( 'theme-light' ); ?></span>
+            </a>
+            <a href="#" data-action="theme" theme="dark">
+              <i class="icon">dark_mode</i>
+              <span class="label"><?php _i18n( 'theme-dark' ); ?></span>
+            </a>
+          </div>
+        </div>
+        <div class="share">
+          <i class="icon">share</i>
+          <span class="label"><?php _i18n( 'share-it' ); ?></span>
+          <?php _share_links(); ?>
+        </div>
+      </div>
+      <div id="warning">
+        <span><?php _i18n( 'site-warning' ); ?></span>
+      </div>
+      <div id="scroll-to-top" data-action="scroll-to-top" title="<?php _i18n( 'scroll-to-top' ); ?>">
+        <i class="icon">arrow_upward</i>
+      </div>
+      <?php if ( ! isset( $_COOKIE[ 'cookie_test' ] ) ) {
+        load_tpl_part( '_cookie' );
+      } ?>
+    </div>
+    <?php _site_end(); ?>
+  </body>
 </html>
--- includes/templates/_header.php
+++ includes/templates/_header.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
 <html lang="<?php _i18n_locale(); ?>">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="author" content="Paul Köhler (komed3)" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-        <meta name="msapplication-TileColor" content="#603cba" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="icon" href="<?php _base_url( 'favicon.ico' ); ?>" />
-        <link rel="icon" type="image/png" sizes="48x48" href="<?php _base_url( 'favicon-48x48.png' ); ?>" />
-        <link rel="icon" type="image/png" sizes="32x32" href="<?php _base_url( 'favicon-32x32.png' ); ?>" />
-        <link rel="icon" type="image/png" sizes="16x16" href="<?php _base_url( 'favicon-16x16.png' ); ?>" />
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php _base_url( 'apple-touch-icon.png' ); ?>" />
-        <link rel="mask-icon" href="<?php _base_url( 'safari-pinned-tab.svg' ); ?>" color="#7050aa" />
-        <link rel="manifest" href="<?php _base_url( 'site.webmanifest' ); ?>" />
-        <?php _site_header(); ?>
-    </head>
-    <body <?php _site_classes(); ?>>
-        <div id="wrapper">
-            <div id="header">
-                <a href="<?php _base_url(); ?>" title="<?php _i18n( 'site-title-default' ); ?>" class="site-logo">
-                    <img src="<?php _base_url( 'favicon-48x48.png' ); ?>" alt="" />
-                </a>
-                <?php _site_nav( [ [
-                    'i18n' => 'site-nav-map',
-                    'url' => '',
-                    'check' => ''
-                ], [
-                    'i18n' => 'site-nav-airports',
-                    'url' => 'airports',
-                    'check' => 'airports'
-                ], [
-                    'i18n' => 'site-nav-weather',
-                    'url' => 'weather',
-                    'check' => 'weather'
-                ], [
-                    'i18n' => 'site-nav-list',
-                    'url' => 'list',
-                    'check' => 'list'
-                ], [
-                    'i18n' => 'site-nav-traffic',
-                    'url' => 'traffic',
-                    'check' => 'traffic'
-                ] ], 'site-nav site-tabs' ); ?>
-                <?php load_tpl_part( '_searchform' ); ?>
-            </div>
-            <div id="content">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Paul Köhler (komed3)" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <meta name="msapplication-TileColor" content="#603cba" />
+    <meta name="theme-color" content="#ffffff" />
+    <link rel="icon" href="<?php _base_url( 'favicon.ico' ); ?>" />
+    <link rel="icon" type="image/png" sizes="48x48" href="<?php _base_url( 'favicon-48x48.png' ); ?>" />
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php _base_url( 'favicon-32x32.png' ); ?>" />
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php _base_url( 'favicon-16x16.png' ); ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php _base_url( 'apple-touch-icon.png' ); ?>" />
+    <link rel="mask-icon" href="<?php _base_url( 'safari-pinned-tab.svg' ); ?>" color="#7050aa" />
+    <link rel="manifest" href="<?php _base_url( 'site.webmanifest' ); ?>" />
+    <?php _site_header(); ?>
+  </head>
+  <body <?php _site_classes(); ?>>
+    <div id="wrapper">
+      <div id="header">
+        <a href="<?php _base_url(); ?>" title="<?php _i18n( 'site-title-default' ); ?>" class="site-logo">
+          <img src="<?php _base_url( 'favicon-48x48.png' ); ?>" alt="" />
+        </a>
+        <?php _site_nav( [ [
+          'i18n' => 'site-nav-map',
+          'url' => '',
+          'check' => ''
+        ], [
+          'i18n' => 'site-nav-airports',
+          'url' => 'airports',
+          'check' => 'airports'
+        ], [
+          'i18n' => 'site-nav-weather',
+          'url' => 'weather',
+          'check' => 'weather'
+        ], [
+          'i18n' => 'site-nav-list',
+          'url' => 'list',
+          'check' => 'list'
+        ], [
+          'i18n' => 'site-nav-traffic',
+          'url' => 'traffic',
+          'check' => 'traffic'
+        ] ], 'site-nav site-tabs' ); ?>
+        <?php load_tpl_part( '_searchform' ); ?>
+      </div>
+      <div id="content">
