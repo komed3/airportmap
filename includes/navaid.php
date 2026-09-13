2ce541df417ee61c8021389a8f1e1dd3f4d93731
komed3 <webmaster@komed3.de>
2026-09-14 00:35:06 +0200

    update navaid.php

--- includes/navaid.php
+++ includes/navaid.php
@@ -1,126 +1,98 @@
 <?php
 
-    function navaid_ident(
-        array $navaid
-    ) {
+  function navaid_ident ( array $navaid ) {
+    return '<morse>' . __morse( $navaid[ 'ident' ] ) . '</morse>';
+  }
 
-        return '<morse>' . __morse( $navaid['ident'] ) . '</morse>';
+  function navaid_level ( array $navaid ) {
+    return i18n( 'level-' . $navaid[ 'level' ] );
+  }
 
-    }
+  function navaid_power ( array $navaid ) {
+    return i18n( 'power-' . $navaid[ 'power' ] );
+  }
 
-    function navaid_level(
-        array $navaid
-    ) {
+  function navaid_list ( array $airport, array $navaids ) {
 
-        return i18n( 'level-' . $navaid['level'] );
+    $list = '';
 
-    }
+    foreach ( $navaids as $navaid ) {
 
-    function navaid_power(
-        array $navaid
-    ) {
-
-        return i18n( 'power-' . $navaid['power'] );
-
-    }
-
-    function navaid_list(
-        array $airport,
-        array $navaids
-    ) {
-
-        $list = '';
-
-        foreach( $navaids as $navaid ) {
-
-            $list .= '<div class="navaid navaid-' . $navaid['type'] . '">
-                <navicon></navicon>
-                <div class="info">
-                    <div class="headline">
-                        <span class="ident">' . $navaid['ident'] . '</span>
-                        <morse>' . __morse( $navaid['ident'] ) . '</morse>
-                    </div>
-                    <div class="freq">' . format_freq( $navaid['frequency'] ) . '</div>
-                    <div class="line">
-                        <span class="type">' . $navaid['type'] . '</span>
-                        <span class="name">' . $navaid['name'] . '</span>
-                    </div>
-                    ' . ( empty( $navaid['level'] ) ? '' : '<div class="line">
-                        <span class="usage">' . i18n( 'level-' . $navaid['level'] ) . '</span>
-                        <span class="power">(' . i18n( 'power-' . $navaid['power'] ) . ')</span>
-                    </div>' ) . '
-                    <div class="line">
-                        ' . __DMS_coords( $navaid['lat'], $navaid['lon'] ) . '
-                    </div>
-                    <div class="line">
-                        <span>' . alt_in( $navaid['alt'], 'ft' ) . '</span>
-                        <span>(' . alt_in( $navaid['alt'] / 3.281, 'm&nbsp;MSL' ) . ')</span>
-                    </div>
-                </div>
-            </div>';
-
-        }
-
-        return '<div class="navaidlist">
-            ' . $list . '
-        </div>';
+      $list .= '<div class="navaid navaid-' . $navaid[ 'type' ] . '">
+        <navicon></navicon>
+        <div class="info">
+          <div class="headline">
+            <span class="ident">' . $navaid[ 'ident' ] . '</span>
+            <morse>' . __morse( $navaid[ 'ident' ] ) . '</morse>
+          </div>
+          <div class="freq">' . format_freq( $navaid[ 'frequency' ] ) . '</div>
+          <div class="line">
+            <span class="type">' . $navaid[ 'type' ] . '</span>
+            <span class="name">' . $navaid[ 'name' ] . '</span>
+          </div>
+          ' . ( empty( $navaid[ 'level' ] ) ? '' : '<div class="line">
+            <span class="usage">' . i18n( 'level-' . $navaid[ 'level' ] ) . '</span>
+            <span class="power">(' . i18n( 'power-' . $navaid[ 'power' ] ) . ')</span>
+          </div>' ) . '
+          <div class="line">
+            ' . __DMS_coords( $navaid[ 'lat' ], $navaid[ 'lon' ] ) . '
+          </div>
+          <div class="line">
+            <span>' . alt_in( $navaid[ 'alt' ], 'ft' ) . '</span>
+            <span>(' . alt_in( $navaid[ 'alt' ] / 3.281, 'm&nbsp;MSL' ) . ')</span>
+          </div>
+        </div>
+      </div>';
 
     }
 
-    function _navaid_list(
-        array $airport,
-        array $navaids
-    ) {
+    return '<div class="navaidlist">
+      ' . $list . '
+    </div>';
 
-        echo navaid_list( $airport, $navaids );
+  }
 
-    }
+  function _navaid_list ( array $airport, array $navaids ) {
+    echo navaid_list( $airport, $navaids );
+  }
 
-    function waypoint_list(
-        array $airport,
-        array $waypoints
-    ) {
+  function waypoint_list ( array $airport, array $waypoints ) {
 
-        $list = '';
+    $list = '';
 
-        foreach( $waypoints as $waypoint ) {
+    foreach ( $waypoints as $waypoint ) {
 
-            $list .= '<div class="waypoint">
-                <wpicon></wpicon>
-                <div class="info">
-                    <div class="headline">
-                        <span class="ident">' . $waypoint['ident'] . '</span>
-                        <morse>' . __morse( $waypoint['ident'] ) . '</morse>
-                    </div>
-                    <div class="line coords">
-                        ' . __DMS_coords( $waypoint['lat'], $waypoint['lon'] ) . '
-                    </div>
-                    <div class="line dist">
-                        <i class="icon">near_me</i>
-                        <span>' . alt_in( $waypoint['distance'], 'nm' ) . '</span>
-                        <span>' . __cardinal( __HDG(
-                            $airport['lat'], $airport['lon'],
-                            $waypoint['lat'], $waypoint['lon']
-                        ) ) . '</span>
-                    </div>
-                </div>
-            </div>';
-
-        }
-
-        return '<div class="waypointlist">
-            ' . $list . '
-        </div>';
+      $list .= '<div class="waypoint">
+        <wpicon></wpicon>
+        <div class="info">
+          <div class="headline">
+            <span class="ident">' . $waypoint[ 'ident' ] . '</span>
+            <morse>' . __morse( $waypoint[ 'ident' ] ) . '</morse>
+          </div>
+          <div class="line coords">
+            ' . __DMS_coords( $waypoint[ 'lat' ], $waypoint[ 'lon' ] ) . '
+          </div>
+          <div class="line dist">
+            <i class="icon">near_me</i>
+            <span>' . alt_in( $waypoint[ 'distance' ], 'nm' ) . '</span>
+            <span>' . __cardinal( __HDG(
+              $airport[ 'lat' ], $airport[ 'lon' ],
+              $waypoint[ 'lat' ], $waypoint[ 'lon' ]
+            ) ) . '</span>
+          </div>
+        </div>
+      </div>';
 
     }
 
-    function _waypoint_list(
-        array $airport,
-        array $waypoints
-    ) {
+    return '<div class="waypointlist">
+      ' . $list . '
+    </div>';
 
-        echo waypoint_list( $airport, $waypoints );
+  }
 
-    }
+  function _waypoint_list ( array $airport, array $waypoints ) {
+    echo waypoint_list( $airport, $waypoints );
+  }
 
 ?>
