f5930d4c7206ab52bf84e563ee2f1b5eff864946
komed3 <webmaster@komed3.de>
2026-09-13 23:46:40 +0200

    update sigmet_info.php

--- includes/api/sigmet_info.php
+++ includes/api/sigmet_info.php
@@ -1,43 +1,34 @@
 <?php
 
-    require_once __DIR__ . '/api.php';
+  require_once __DIR__ . '/api.php';
 
-    if( !load_requirements( 'language', 'content', 'weather' ) ) {
+  if ( ! load_requirements( 'language', 'content', 'weather' ) )
+    api_exit( [ 'raw' => null, 'infobox' => null ] );
 
-        api_exit( [
-            'raw' => null,
-            'infobox' => null
-        ] );
+  i18n_load( $_POST[ 'locale' ] ?? LOCALE );
 
-    }
+  $res = $DB->query( '
+    SELECT  *
+    FROM    ' . DB_PREFIX . 'sigmet
+    WHERE   _id = ' . $_POST[ 'sigmet' ]
+  );
 
-    i18n_load( $_POST['locale'] ?? LOCALE );
+  if ( $res->num_rows == 1 && $sigmet = $res->fetch_assoc() ) {
 
-    $res = $DB->query( '
-        SELECT  *
-        FROM    ' . DB_PREFIX . 'sigmet
-        WHERE   _id = ' . $_POST['sigmet']
-    );
+    $infobox = [
+      'title' => sigmet_hazard( $sigmet ),
+      'subtitle' => $sigmet[ 'name' ],
+      'content' => '<div class="rawtxt">' . $sigmet[ 'raw' ] . '</div>
+      <hr />
+      <ul class="infobox-list">
+        ' . sigmet_info( $sigmet, false ) . '
+      </ul>',
+      'link' => SITE . 'weather/sigmets',
+      'linktext' => i18n( 'view-sigmets' )
+    ];
 
-    if( $res->num_rows == 1 && $sigmet = $res->fetch_assoc() ) {
+  }
 
-        $infobox = [
-            'title' => sigmet_hazard( $sigmet ),
-            'subtitle' => $sigmet['name'],
-            'content' => '<div class="rawtxt">' . $sigmet['raw'] . '</div>
-            <hr />
-            <ul class="infobox-list">
-                ' . sigmet_info( $sigmet, false ) . '
-            </ul>',
-            'link' => SITE . 'weather/sigmets',
-            'linktext' => i18n( 'view-sigmets' )
-        ];
-
-    }
-
-    api_exit( [
-        'raw' => $sigmet,
-        'infobox' => $infobox ?? null
-    ] );
+  api_exit( [ 'raw' => $sigmet, 'infobox' => $infobox ?? null ] );
 
 ?>
