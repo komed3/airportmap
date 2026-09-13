493613dd02e3a188e2f6e845ecdaa716c6417c60
komed3 <webmaster@komed3.de>
2026-09-14 01:02:41 +0200

    update language.php and map.php

--- includes/language.php
+++ includes/language.php
@@ -1,89 +1,59 @@
 <?php
 
-    $__i18n_locale = $_COOKIE['locale'] ?? LOCALE;
-    $__i18n_msg = [];
+  $__i18n_locale = $_COOKIE[ 'locale' ] ?? LOCALE;
+  $__i18n_msg = [];
 
-    function i18n(
-        string $msgkey,
-        ...$replaces
-    ) {
+  function i18n ( string $msgkey, ...$replaces ) {
 
-        global $__i18n_msg;
+    global $__i18n_msg;
 
-        $key = trim( strtolower( $msgkey ) );
-        $msg = array_key_exists( $key, $__i18n_msg )
-            ? $__i18n_msg[ $key ] : '›' . $msgkey . '‹';
+    $key = trim( strtolower( $msgkey ) );
+    $msg = array_key_exists( $key, $__i18n_msg ) ? $__i18n_msg[ $key ] : '›' . $msgkey . '‹';
 
-        foreach( $replaces as $idx => $replace ) {
+    foreach ( $replaces as $idx => $replace )
+      $msg = str_replace( '$' . ( $idx + 1 ), $replace, $msg );
 
-            $msg = str_replace( '$' . ( $idx + 1 ), $replace, $msg );
+    return $msg;
 
-        }
+  }
 
-        return $msg;
+  function i18n_save ( string $msgkey, ...$replaces ) {
+    return strpos( $msg = i18n( $msgkey, ...$replaces ), '›' ) === 0 ? null : $msg;
+  }
 
-    }
+  function _i18n ( string $msgkey, ...$replaces ) {
+    echo i18n( $msgkey, ...$replaces );
+  }
 
-    function i18n_save(
-        string $msgkey,
-        ...$replaces
-    ) {
+  function i18n_load ( string $lang = '', bool $set_cookie = false ) {
 
-        return strpos(
-            $msg = i18n( $msgkey, ...$replaces ), '›'
-        ) === 0 ? null : $msg;
+    global $__i18n_locale, $__i18n_msg;
 
-    }
+    $load_lang = $__i18n_locale;
 
-    function _i18n(
-        string $msgkey,
-        ...$replaces
-    ) {
+    if ( strlen( $lang ) && is_readable( LANG . $lang . '.min.json' ) ) {
 
-        echo i18n( $msgkey, ...$replaces );
+      $load_lang = $lang;
+
+      if ( !! ( $_COOKIE[ 'cookie_test' ] ?? 0 ) && $set_cookie )
+        setcookie( 'locale', $load_lang, COOKIE_EXP );
 
     }
 
-    function i18n_load(
-        string $lang = '',
-        bool $set_cookie = false
-    ) {
+    $__i18n_msg = json_decode( file_get_contents( LANG . $load_lang . '.json' ), true );
 
-        global $__i18n_locale, $__i18n_msg;
+  }
 
-        $load_lang = $__i18n_locale;
+  function i18n_locale () {
 
-        if( strlen( $lang ) && is_readable( LANG . $lang . '.min.json' ) ) {
+    global $__i18n_locale;
 
-            $load_lang = $lang;
+    return $__i18n_locale;
 
-            if( !!( $_COOKIE['cookie_test'] ?? 0 ) && $set_cookie ) {
+  }
 
-                setcookie( 'locale', $load_lang, COOKIE_EXP );
-
-            }
-
-        }
-
-        $__i18n_msg = json_decode(
-            file_get_contents( LANG . $load_lang . '.json' ),
-            true
-        );
-
-    }
-
-    function i18n_locale() {
-
-        global $__i18n_locale;
-
-        return $__i18n_locale;
-
-    }
-
-    function _i18n_locale() {
-
-        echo i18n_locale();
-
-    }
+  function _i18n_locale () {
+    echo i18n_locale();
+  }
 
 ?>
