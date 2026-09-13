7a55f4055068aed76a229f35002fa8101c1167ae
komed3 <webmaster@komed3.de>
2026-09-14 00:35:09 +0200

    update apm.php

--- includes/apm.php
+++ includes/apm.php
@@ -1,88 +1,69 @@
 <?php
 
-    /* set defaults */
+  /* set defaults */
 
-    date_default_timezone_set( 'UTC' );
-    error_reporting( 0 );
+  date_default_timezone_set( 'UTC' );
+  error_reporting( 0 );
 
-    /* load config */
+  /* load config */
 
-    require_once __DIR__ . '/../config.php';
+  require_once __DIR__ . '/../config.php';
 
-    /* load required files */
+  /* load required files */
 
-    require_once PATH . 'language.php';
-    require_once PATH . 'airport.php';
-    require_once PATH . 'weather.php';
-    require_once PATH . 'navaid.php';
-    require_once PATH . 'content.php';
-    require_once PATH . 'map.php';
+  require_once PATH . 'language.php';
+  require_once PATH . 'airport.php';
+  require_once PATH . 'weather.php';
+  require_once PATH . 'navaid.php';
+  require_once PATH . 'content.php';
+  require_once PATH . 'map.php';
 
-    /* open DB connection */
+  /* open DB connection */
 
-    $DB = new Mysqli(
-        DB_HOST,
-        DB_USER, DB_PASSWORD,
-        DB_NAME, DB_PORT
-    );
+  $DB = new Mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT );
+  $DB->set_charset( DB_CHARSET );
 
-    $DB->set_charset( DB_CHARSET );
+  /* airport count by type */
 
-    /* airport count by type */
+  define( 'AIRPORT_STATS', airport_count() );
+  define( 'AIRPORT_ALL', array_sum( AIRPORT_STATS ) );
 
-    define( 'AIRPORT_STATS', airport_count() );
-    define( 'AIRPORT_ALL', array_sum( AIRPORT_STATS ) );
+  /* load basic resources */
 
-    /* load basic resources */
+  add_resource( 'leaflet', 'css', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css' );
+  add_resource( 'base', 'css', 'base.min.css' );
 
-    add_resource( 'leaflet', 'css', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css' );
-    add_resource( 'base', 'css', 'base.min.css' );
+  add_resource( 'jquery', 'js', 'https://code.jquery.com/jquery-3.6.1.min.js' );
+  add_resource( 'jquery-cookie', 'js', 'jquery.cookie.js' );
+  add_resource( 'leaflet', 'js', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.js' );
+  add_resource( 'base', 'js', 'base.min.js' );
 
-    add_resource( 'jquery', 'js', 'https://code.jquery.com/jquery-3.6.1.min.js' );
-    add_resource( 'jquery-cookie', 'js', 'jquery.cookie.js' );
-    add_resource( 'leaflet', 'js', 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.js' );
-    add_resource( 'base', 'js', 'base.min.js' );
+  /* fetch path */
 
-    /* fetch path */
+  $path = array_values( array_filter( explode( '/', $_SERVER[ 'REQUEST_URI' ] ), function ( $p ) {
+    return ( $p !== null && $p !== false && $p !== '' );
+  } ) );
 
-    $path = array_values( array_filter( explode( '/', $_SERVER['REQUEST_URI'] ), function( $p ) {
-        return ( $p !== null && $p !== false && $p !== '' );
-    } ) );
+  /* load language messages */
 
-    /* load language messages */
+  if ( in_array( $path[ 0 ] ?? '', LANGUAGES ) ) i18n_load( array_shift( $path ), true );
+  else i18n_load();
 
-    if( in_array( $path[0] ?? '', LANGUAGES ) ) {
+  /* load template */
 
-        i18n_load( array_shift( $path ), true );
+  if ( is_readable( TEMPLATE . ( $tpl = strtolower( $path[ 0 ] . '_' . $path[ 1 ] ) ) . '.php' ) )
+    require_once TEMPLATE . $tpl . '.php';
 
-    } else {
+  else if( is_readable( TEMPLATE . ( $tpl = strtolower( $path[ 0 ] ?? 'map' ) ) . '.php' ) )
+    require_once TEMPLATE . $tpl . '.php';
 
-        i18n_load();
+  else if( ! empty( $airport = airport_by( 'ICAO', $path[ 0 ] ?? '' ) ) )
+    __redirect( 'airport/' . $airport[ 'ICAO' ] );
 
-    }
+  else require_once TEMPLATE . '404.php';
 
-    /* load template */
+  /* close DB connection */
 
-    if( is_readable( TEMPLATE . ( $tpl = strtolower( $path[0] . '_' . $path[1] ) ) . '.php' ) ) {
-
-        require_once TEMPLATE . $tpl . '.php';
-
-    } else if( is_readable( TEMPLATE . ( $tpl = strtolower( $path[0] ?? 'map' ) ) . '.php' ) ) {
-
-        require_once TEMPLATE . $tpl . '.php';
-
-    } else if( !empty( $airport = airport_by( 'ICAO', $path[0] ?? '' ) ) ) {
-
-        __redirect( 'airport/' . $airport['ICAO'] );
-
-    } else {
-
-        require_once TEMPLATE . '404.php';
-
-    }
-
-    /* close DB connection */
-
-    $DB->close();
+  $DB->close();
 
 ?>
