927649a2f53ca471b35e49df778f006e2eebc683
komed3 <webmaster@komed3.de>
2026-09-14 02:09:06 +0200

    update base.js and base.min.js

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,199 +1,137 @@
-var baseurl = window.location.origin,
-    resurl = baseurl + '/static/resources/',
-    apiurl = baseurl + '/includes/api/',
-    use_cookies = false;
+const baseurl = window.location.origin,
+      resurl = baseurl + '/static/resources/',
+      apiurl = baseurl + '/includes/api/',
+      use_cookies = false;
 
-var get_token = () => {
+const get_token = () => self.crypto.randomUUID();
 
-    return self.crypto.randomUUID();
-
+const prevent = ( e ) => {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
 };
 
-var prevent = ( e ) => {
+const number_format = ( number, options = {} ) => new Intl.NumberFormat(
+  $.cookie( 'locale' ) || 'en', options
+).format( number );
 
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
+const freq_format = ( frequency ) => {
+  frequency = parseInt( frequency );
 
+  return frequency > 1000 ? number_format( frequency / 1000, {
+    minimumFractionDigits: 1, maximumFractionDigits: 3
+  } ) + '&#8239;MHz' : number_format( frequency ) + '&#8239;kHz';
 };
 
-var number_format = ( number, options = {} ) => {
+( function ( $ ) {
+  $( document ).ready( function () {
+    $.cookie.defaults = { path: '/', expires: 365 };
 
-    return ( new Intl.NumberFormat(
-        $.cookie( 'locale' ) || 'en',
-        options
-    ) ).format( number );
+    if ( 'serviceWorker' in navigator ) navigator.serviceWorker.register( baseurl + '/service-worker.js', { scope: '/' } );
+    if ( !! ( $.cookie( 'cookie_test' ) || 0 ) ) use_cookies = true;
 
-}
+    $( 'a[target="_blank"]' ).each( function () { $( this ).attr( 'rel', 'noopener noreferrer' ) } );
+  } );
 
-var freq_format = ( frequency ) => {
+  $( document ).on( 'click', '[data-action]', function ( e ) {
+    prevent( e );
 
-    frequency = parseInt( frequency );
+    switch ( $( this ).attr( 'data-action' ) ) {
+      case 'scroll-to-top':
+        $( 'html, body' ).animate( { scrollTop: 0 }, 'fast' );
+        break;
 
-    return frequency > 1000
-        ? number_format( frequency / 1000, {
-            minimumFractionDigits: 1,
-            maximumFractionDigits: 3
-        } ) + '&#8239;MHz'
-        : number_format( frequency ) + '&#8239;kHz';
+      case 'cookie':
+        $.cookie( 'cookie_test', +!! parseInt( $( this ).attr( 'data-cookie' ) ) );
+        location.reload();
+        break;
 
-};
+      case 'theme':
+        $.cookie( 'theme', $( this ).attr( 'theme' ) );
+        location.reload();
+        break;
 
-( function( $ ) {
+      case 'share':
+        window.open( {
+          twitter: 'https://twitter.com/intent/tweet?text={TEXT}&url={URL}',
+          facebook: 'https://www.facebook.com/sharer/sharer.php?u={URL}&quote={TEXT}',
+          telegram: 'https://t.me/share/url?url={URL}&text={TEXT}',
+          tumblr: 'https://www.tumblr.com/widgets/share/tool?canonicalUrl={URL}&caption={TEXT}',
+          reddit: 'https://www.reddit.com/submit?url={URL}&title={TEXT}'
+        }[ $( this ).attr( 'data-site' ) ]
+          .replace( '{URL}', $( this ).attr( 'data-url' ) )
+          .replace( '{TEXT}', $( this ).attr( 'data-text' ) ),
+          '_blank'
+        );
+        break;
 
-    $( document ).ready( function() {
-
-        $.cookie.defaults = {
-            path: '/',
-            expires: 365
-        };
-
-        if( 'serviceWorker' in navigator ) {
-
-            navigator.serviceWorker.register( baseurl + '/service-worker.js', { scope: '/' } );
-
-        }
-
-        if( !!( $.cookie( 'cookie_test' ) || 0 ) ) {
-
-            use_cookies = true;
-
-        }
-
-        $( 'a[target="_blank"]' ).each( function() {
-
-            $( this ).attr( 'rel', 'noopener noreferrer' );
-
+      case 'vicinity-my':
+        navigator.geolocation.getCurrentPosition( ( { coords: { latitude, longitude } } ) => {
+          location.href = baseurl + '/vicinity/' +
+            ( Math.round( latitude * 100000 ) / 100000 ) + '/' +
+            ( Math.round( longitude * 100000 ) / 100000 );
         } );
+        break;
+    }
+  } );
 
-    } );
+  $( document ).on( 'change', '[data-action]', function ( e ) {
+    prevent( e );
 
-    $( document ).on( 'click', '[data-action]', function( e ) {
+    switch ( $( this ).attr( 'data-action' ) ) {
+      case 'select-language':
+        $.cookie( 'locale', $( this ).val().toString().trim() );
+        location.reload();
+        break;
 
-        prevent( e );
+      case 'select-page':
+        location.href = ( $( this ).attr( 'data-base' ) || baseurl + '/' ) +
+          $( this ).val().toString().trim() + '#' + ( $( this ).attr( 'data-jump' ) || '' );
+        break;
 
-        switch( $( this ).attr( 'data-action' ) ) {
+      case 'select-letter':
+        location.href = baseurl + '/list/' + $( this ).val().toString().trim();
+        break;
 
-            case 'scroll-to-top':
-                $( 'html, body' ).animate( {
-                    scrollTop: 0
-                }, 'fast' );
-                break;
+      case 'select-station':
+        location.href = baseurl + '/' + $( this ).attr( 'data-base' ) + $( this ).val() + (
+          ( jump = $( this ).attr( 'data-jump' ) ) ? '#' + jump : ''
+        );
+        break;
+    }
+  } );
 
-            case 'cookie':
-                $.cookie( 'cookie_test', +!!parseInt( $( this ).attr( 'data-cookie' ) ) );
-                location.reload();
-                break;
+  $( document ).on( 'submit', '[data-form]', function ( e ) {
+    prevent( e );
 
-            case 'theme':
-                $.cookie( 'theme', $( this ).attr( 'theme' ) );
-                location.reload();
-                break;
+    switch ( $( this ).attr( 'data-form' ) ) {
+      case 'search':
+        location.href = baseurl + '/search/' + encodeURI(
+          ( $( this ).find( '[name="searchtext"]' ).val() || '' ).toString().trim()
+        );
+        break;
 
-            case 'share':
-                window.open(
-                    {
-                        twitter: 'https://twitter.com/intent/tweet?text={TEXT}&url={URL}',
-                        facebook: 'https://www.facebook.com/sharer/sharer.php?u={URL}&quote={TEXT}',
-                        telegram: 'https://t.me/share/url?url={URL}&text={TEXT}',
-                        tumblr: 'https://www.tumblr.com/widgets/share/tool?canonicalUrl={URL}&caption={TEXT}',
-                        reddit: 'https://www.reddit.com/submit?url={URL}&title={TEXT}'
-                    }[ $( this ).attr( 'data-site' ) ]
-                        .replace( '{URL}', $( this ).attr( 'data-url' ) )
-                        .replace( '{TEXT}', $( this ).attr( 'data-text' ) ),
-                    '_blank'
-                );
-                break;
+      case 'embed':
+        location.href = baseurl + '/embed/' +
+          ( $( this ).find( '[name="code"]' ).val() || '' ).toString().trim().toUpperCase() + '/' +
+          ( $( this ).find( '[name="lang"]' ).val() || '' ).toString().trim().toLowerCase() + '/' +
+          +( $( this ).find( '[name="weather"]' ).is( ':checked' ) ) + '/' +
+          +( $( this ).find( '[name="stats"]' ).is( ':checked' ) ) + '/' +
+          +( $( this ).find( '[name="image"]' ).is( ':checked' ) );
+        break;
 
-            case 'vicinity-my':
-                navigator.geolocation.getCurrentPosition( ( position ) => {
+      case 'vicinity':
+        location.href = baseurl + '/vicinity/' +
+          parseFloat( $( this ).find( '[name="lat"]' ).val() || 0 ) + '/' +
+          parseFloat( $( this ).find( '[name="lon"]' ).val() || 0 );
+        break;
+    }
 
-                    location.href = baseurl + '/vicinity/' +
-                        ( Math.round( position.coords.latitude * 100000 ) / 100000 ) + '/' +
-                        ( Math.round( position.coords.longitude * 100000 ) / 100000 );
+    return false;
+  } );
 
-                } );
-                break;
-
-        }
-
-    } );
-
-    $( document ).on( 'change', '[data-action]', function( e ) {
-
-        prevent( e );
-
-        switch( $( this ).attr( 'data-action' ) ) {
-
-            case 'select-language':
-                $.cookie( 'locale', $( this ).val().toString().trim() );
-                location.reload();
-                break;
-
-            case 'select-page':
-                location.href = ( $( this ).attr( 'data-base' ) || baseurl + '/' ) +
-                    $( this ).val().toString().trim() + '#' + ( $( this ).attr( 'data-jump' ) || '' );
-                break;
-
-            case 'select-letter':
-                location.href = baseurl + '/list/' + $( this ).val().toString().trim();
-                break;
-
-            case 'select-station':
-                location.href = baseurl + '/' + $( this ).attr( 'data-base' ) + $( this ).val() + (
-                    ( jump = $( this ).attr( 'data-jump' ) ) ? '#' + jump : ''
-                );
-                break;
-
-        }
-
-    } );
-
-    $( document ).on( 'submit', '[data-form]', function( e ) {
-
-        prevent( e );
-
-        switch( $( this ).attr( 'data-form' ) ) {
-
-            case 'search':
-                location.href = baseurl + '/search/' + encodeURI(
-                    ( $( this ).find( '[name="searchtext"]' ).val() || '' ).toString().trim()
-                );
-                break;
-
-            case 'embed':
-                location.href = baseurl + '/embed/' +
-                    ( $( this ).find( '[name="code"]' ).val() || '' ).toString().trim().toUpperCase() + '/' +
-                    ( $( this ).find( '[name="lang"]' ).val() || '' ).toString().trim().toLowerCase() + '/' +
-                    +( $( this ).find( '[name="weather"]' ).is( ':checked' ) ) + '/' +
-                    +( $( this ).find( '[name="stats"]' ).is( ':checked' ) ) + '/' +
-                    +( $( this ).find( '[name="image"]' ).is( ':checked' ) );
-                break;
-
-            case 'vicinity':
-                location.href = baseurl + '/vicinity/' +
-                    parseFloat( $( this ).find( '[name="lat"]' ).val() || 0 ) + '/' +
-                    parseFloat( $( this ).find( '[name="lon"]' ).val() || 0 );
-                break;
-
-        }
-
-        return false;
-
-    } );
-
-    $( document ).scroll( function() {
-
-        if( $( window ).scrollTop() > 200 ) {
-
-            $( 'body' ).addClass( 'scroll-to-top' );
-
-        } else {
-
-            $( 'body' ).removeClass( 'scroll-to-top' );
-
-        }
-
-    } );
-
+  $( document ).scroll( function () {
+    if( $( window ).scrollTop() > 200 ) $( 'body' ).addClass( 'scroll-to-top' );
+    else $( 'body' ).removeClass( 'scroll-to-top' );
+  } );
 } )( jQuery );
